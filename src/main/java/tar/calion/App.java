4d0bb7c6acc690a45bd752f5e9c10eca90248de8
Tar-Calion <43185234+Tar-Calion@users.noreply.github.com>
2024-02-18 23:33:51 +0100

    Create project

---
+++ src/main/java/tar/calion/App.java
@@ -0,0 +1,17 @@
+package tar.calion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        getPrintln();
+    }
+
+    private static void getPrintln() {
+        System.out.println( "Hello World!" );
+    }
+}
